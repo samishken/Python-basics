f1efdcd3ebdea2413214ecdbfe7e0144066c93ab
samishken <samishken@gmail.com>
2019-10-23 13:14:17 -0400

    countries list practice

---
+++ countriesList.py
@@ -0,0 +1,30 @@
+# ListType
+countries=["England", "Uganda", "Brazil"]
+print(countries)
+
+#add to the list
+countries.insert(3, "Japan")
+
+# remove from the list
+countries.remove('Brazil')
+
+# insert to list at index 1
+countries.insert(1, "Peru")
+print(countries)
+
+#SetType
+set={"Ireland", "Kenya", "Mexico"}
+print(set)
+
+for i in set:
+    print(set)
+
+# add element to set
+set.update(["Korea"])
+
+# remove element
+set.remove("Kenya")
+
+# add element at index 1
+set.add("Ghana")
+print(set)
