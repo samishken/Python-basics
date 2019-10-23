5200f2dd1c4e1ad5023b018bab9972a86337e9d0
samishken <samishken@gmail.com>
2019-10-23 00:05:39 -0400

    add rangetypes examples

---
+++ rangetype.py
@@ -0,0 +1,8 @@
+r=range(5) # 01234
+r=range(1,6) # 12345  donot include the max amount
+r=range(1,15,3) # 1 4 7 10 13  skips 2 number in between
+
+#For each value inside r (0-5) will be assigned to "i"
+for i in r:
+    print(i)
+
