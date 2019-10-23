50d0a4fb7661ffc3c8e4ec33b0b4b47c06df0429
samishken <samishken@gmail.com>
2019-10-22 23:43:48 -0400

    tuple examples added

--- tupletype.py
+++ tupletype.py
@@ -4,5 +4,25 @@ We can't use the following methods in Tuple
 append, extend, insert, remove, clear
 """
 
-tpl=()
+tpl=(40, 50, 50, 'xyz')
 print(tpl)
+
+tpl=(40,)
+# with comma its considered as tuple
+print(type(tpl))
+tpl=(40)
+# without comma its considered as integer
+print(type(tpl))
+
+tpl=(40, 50, 40, 'xyz')
+print(tpl)   # print all elements in the tuple
+print(tpl[3])  # give me the element that's is the third index
+print(tpl*3) # multiply the tuple 3 times
+print(tpl.count(40))  # how many 40 inside a tuple
+print(tpl.index('xyz'))  # what's the index number of that element
+
+lst=[67, 54, 'xyz']
+print(type(lst))
+
+tpl1=tuple(lst)
+print(type(tpl1))
