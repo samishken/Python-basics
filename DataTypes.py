ea4901d0e03dd86df800c6c798c941a9f23b737a
samishken <samishken@gmail.com>
2019-10-22 20:21:03 -0400

    numeric types

---
+++ DataTypes.py
@@ -0,0 +1,9 @@
+"""
+Five DataTypes
+1) None Type
+2) Numeric
+3) Sequences
+4) Sets
+5) Mappings
+"""
+
---
+++ NumericType.py
@@ -0,0 +1,66 @@
+#INT <class 'int'>
+a=13
+b=100
+c=-66
+# Prints all values 13 100 -66
+print(a,b,c)
+print(type(a))
+
+#FLOAT  <class 'float'>
+x=33.6
+y=25.8
+z=20.5
+print(x,y,z)
+# What type of a variable is a particular variable
+print(type(z))
+
+#COMPLEX <class 'complex'>
+d=3+5j
+print(d)
+print(type(d))
+
+#Binary
+e=0B1010
+print(e)
+print(type(e))
+
+#Hexadecimal
+f=0XFF
+print(f)
+print(type(f))
+
+#Ocatal
+h=0o20
+print(h)
+
+
+#Boolean
+g=True
+print(g)
+print(type(g))
+print(9>8)
+print(9<8)
+
+#Conversion types
+
+h=int(x)
+#converts decimal 33.5 to int 33
+print(h)
+print(type(h))
+
+i=float("22.5")
+#convert 'i' which is a string to float <class 'float'>
+print(i)
+print(type(i))
+
+
+print(oct((10)))
+print(hex((10)))
+print(bin((10)))
+
+
+
+
+
+
+
