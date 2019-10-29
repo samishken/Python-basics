5b9f37ea99d6dea8e5fe31204f0c4b5099159a47
samishken <samishken@gmail.com>
2019-10-29 00:38:01 -0400

    functions

---
+++ Functions/average.py
@@ -0,0 +1,13 @@
+def average(a,b):
+    print("Average of two numbers",(a+b)/2)
+average(10,20)
+
+def average(a,b):
+    return (a+b)/2
+result = average(10,20)
+print(result)
+
+def average(a,b):
+    return (a+b)/2
+print("Print average",average(10,20))
+
---
+++ Functions/calcTwoValues.py
@@ -0,0 +1,14 @@
+def calc(a,b):
+    x=a+b
+    y=a-b
+    z=a*b
+    u=a/b
+    return x,y,z,u
+
+result = calc(10,5)
+print(result)
+print(type(result))
+
+for i in result:
+    print(i)
+    print(type(i))
