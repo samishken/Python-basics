9d54390fe3adadffffe055c6e183d714b4ee7476
samishken <samishken@gmail.com>
2019-10-29 00:11:47 -0400

    commandline arguments

---
+++ CommandLine/argumentsDemo.py
@@ -0,0 +1,10 @@
+import sys
+lst =sys.argv
+# To Display the CL arguments
+for i in lst:
+    print(i)
+# to display the length of arguments
+print(len(lst))
+
+# Script/program name
+print(lst[0])
---
+++ CommandLine/productCommnadLine.py
@@ -0,0 +1,7 @@
+import sys
+
+lst = sys.argv
+# Pass Parameter 5 6  to print out 30
+print("Print is:",int(lst[1])*int(lst[2]))
+
+# Print is: 30
